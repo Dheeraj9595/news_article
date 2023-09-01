ae5ce3ed2e0d1bbe7f56ed38dca263e8d2b0e030
Dheeraj Pandey <121604445+Dheeraj9595@users.noreply.github.com>
2023-09-01 15:34:13 +0530

    Wsgi get error fixed

--- news_article/settings.py
+++ news_article/settings.py
@@ -76,24 +76,24 @@ WSGI_APPLICATION = 'news_article.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'NAME': 'newsapp',
-        'HOST': 'LOCALHOST',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'USER': 'root',
+#         'PASSWORD': '1234',
+#         'NAME': 'newsapp',
+#         'HOST': 'LOCALHOST',
+#         'PORT': '3306',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
--- newsapp/views.py
+++ newsapp/views.py
@@ -14,6 +14,7 @@ from .models import UserSearch
 
 API_KEY = "c3c9a565056c49e6923348aea449ff6d"
 
+
 @login_required(login_url='login')
 def fetch_news(request):
     articles = []
@@ -52,42 +53,10 @@ def fetch_news(request):
         'recent_searches': recent_searches
     })
 
-# fetch news only simple
-# API_ENDPOINT = "https://newsapi.org"
-# API_ENDPOINT = "https://newsapi.org"
-# API_KEY = "c3c9a565056c49e6923348aea449ff6d"
-
-# def fetch_news(request):
-#     articles = []
-#     keyword = ""
-
-#     # (POST request)
-#     if request.method == 'POST':
-#         keyword = request.POST.get('keyword', '')
-#         url = 'https://newsapi.org/v2/everything'
-#         params = {
-#             'q': keyword,
-#             'sortBy': 'popularity',
-#             'apiKey': API_KEY
-#         }
-
-#         response = requests.get(url, params=params)
-#         data = response.json()
-#         articles = data.get('articles', [])
-
-#         # Format the date for each article
-#         for article in articles:
-#             dt = datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
-#             article['publishedAtFormatted'] = dt.strftime('%Y-%m-%d %H:%M:%S')
-
-#         # Save the user's search query to the database
-#         if request.user.is_authenticated:
-#             UserSearch.objects.create(user=request.user, query=keyword)
-
-#     return render(request, 'Search_news.html', {'articles': articles, 'keyword': keyword})
-
 
 # ------------------------------------------------------------------------------------------
+
+
 API_ENDPOINT = 'https://newsapi.org/v2/top-headlines'
 
 @login_required(login_url='login')
@@ -106,9 +75,10 @@ def headlines(request):
     if data['status'] == 'ok':
         articles = data['articles']
         for article in articles:
-            dt = datetime.strptime(
-                article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
-            article['publishedAtFormatted'] = dt.strftime('%Y-%m-%d %H:%M:%S')
+            published_at = article.get('publishedAt', None)
+            if published_at:
+                dt = datetime.strptime(published_at, '%Y-%m-%dT%H:%M:%SZ')
+                article['publishedAtFormatted'] = dt.strftime('%Y-%m-%d %H:%M:%S')
 
     paginator = Paginator(articles, 10)  # Show 10 articles per page
 
@@ -130,51 +100,10 @@ def headlines(request):
     return render(request, 'headlines.html', context)
 
 
-# API_ENDPOINT = 'https://newsapi.org/v2/top-headlines'
-
-# def headlines(request):
-#     keyword = request.GET.get('keyword', '')
-#     sort_by = request.GET.get('sort_by', 'popularity')
-#     articles = []
-
-#     params = {'country': 'in', 'apiKey': API_KEY, 'sortBy': sort_by}
-#     if keyword:
-#         params['q'] = keyword
-
-#     response = requests.get(API_ENDPOINT, params=params)
-#     data = response.json()
-
-#     if data['status'] == 'ok':
-#         articles = data['articles']
-#         for article in articles:
-#             dt = datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
-#             article['publishedAtFormatted'] = dt.strftime('%Y-%m-%d %H:%M:%S')
-
-#         # Sort the articles by date (newest first) before passing them to the paginator
-#         articles.sort(key=lambda x: x['publishedAt'], reverse=True)
-
-#     paginator = Paginator(articles, 10)  # Show 10 articles per page
-
-#     page = request.GET.get('page')
-#     try:
-#         articles_page = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If page is not an integer, deliver the first page.
-#         articles_page = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range, deliver the last page of results.
-#         articles_page = paginator.page(paginator.num_pages)
-
-#     context = {
-#         'keyword': keyword,
-#         'articles': articles_page,
-#     }
-
-#     return render(request, 'headlines.html', context)
-
 
 # ------------------------------------------------------------------------------------------
 SOURCES_ENDPOINT = 'https://newsapi.org/v2/top-headlines/sources'
+
 
 @login_required(login_url='login')
 def sources(request):
@@ -194,4 +123,4 @@ def sources(request):
 # ------------------------------------------------------------------------------------------
 @login_required(login_url='login')
 def home2(request):
-    return render(request,"home.html")
+    return render(request, "home.html")
